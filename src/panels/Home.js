1114b43578aa4cd04f47c125a78012e8cdfb41f8
Yokomi <38473135+Yokomide@users.noreply.github.com>
2021-04-13 02:48:17 +0300

    .

---
+++ src/panels/Home.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+import Header from '@vkontakte/vkui/dist/components/Header/Header';
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+
+const Home = ({ id, go, fetchedUser }) => (
+	<Panel id={id}>
+		<PanelHeader>Цифровой прорыв: Off-Class</PanelHeader>
+		{fetchedUser &&
+		<Group header={<Header mode="secondary">Профиль</Header>}>
+			<Cell
+				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+			>
+				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+			</Cell>
+		</Group>}
+
+		<Group header={<Header mode="secondary">Пример кнопок навигации</Header>}>
+			<Div>
+				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
+					Показать Персика
+				</Button>
+			</Div>
+			<Div>
+				<Button stretched size="l" mode="secondary" onClick={go} data-to="spotty">
+					Ещё какая-то кнопка
+				</Button>
+			</Div>
+			
+		</Group>
+
+		<Group header={<Header mode="secondary">Пример 2</Header>}>		
+		<Div>
+				<Button stretched size="l" mode="secondary" onClick={go} data-to="spotty">
+					Ну и ещё, только под новым заголовком
+				</Button>
+			</Div>
+			</Group>
+	</Panel>
+);
+
+Home.propTypes = {
+	id: PropTypes.string.isRequired,
+	go: PropTypes.func.isRequired,
+	fetchedUser: PropTypes.shape({
+		photo_200: PropTypes.string,
+		first_name: PropTypes.string,
+		last_name: PropTypes.string,
+		city: PropTypes.shape({
+			title: PropTypes.string,
+		}),
+	}),
+};
+
+export default Home;
