62318d018e09252babe233153fc91831296fc68a
Yokomi <38473135+Yokomide@users.noreply.github.com>
2021-04-13 03:36:45 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -6,6 +6,7 @@ import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader
 import Header from '@vkontakte/vkui/dist/components/Header/Header';
 import Button from '@vkontakte/vkui/dist/components/Button/Button';
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
 
